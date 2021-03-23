47cd133315d5e24d80ac0c14ec972e88a64bd7b4
lolkindom <pjandj@mail.ru>
2021-03-23 19:18:31 +0300

    add assert

--- For HH 2.1.py
+++ For HH 2.1.py
@@ -1,35 +1,57 @@
-a = []
-first, last = -1, -1
-size = 5
+class ListIsEmptyError(Exception):
+    pass
 
 
-def push(value):
-    isFull()
-    a.append(value)
-    global last
-    last += 1
+class ListIsFullError(Exception):
+    pass
 
 
-def pop():
-    global first
-    first += 1
-    return a[first]
+class Fifo:
+    def __init__(self, limit=5):
+        self.head, self.tail = -1, -1
+        self.count = 0
+        self.limit = limit
+        self.circle = [None]*self.limit
+
+    def is_full(self):
+        return self.count == self.limit
+
+    def is_empty(self):
+        return self.count == 0
+
+    def pop(self):
+        if self.is_empty():
+            raise ListIsEmptyError
+        buff = self.circle[self.head]
+        if self.head == self.tail:
+            self.head, self.tail = -1, -1
+        else:
+            self.count -= 1
+            self.head = (self.head + 1) % self.limit
+        return buff
+
+    def push(self, a):
+        if self.is_full():
+            raise ListIsFullError
+        if self.is_empty():
+            self.head += 1
+            self.tail += 1
+            self.count += 1
+            self.circle[self.head] = a
+        else:
+            self.count += 1
+            self.tail = (self.tail + 1) % self.limit
+            self.circle[self.tail] = a
 
 
-def isFull():
-    if len(a) == size:
-        return BufferError
+if __name__ == '__main__':
+    a = Fifo(3)
+    a.push(1)
+    a.push(2)
+    a.push(3)
+    assert a.pop() == 1
+    a.push(4)
+    assert a.pop() == 2
 
 
-
-push(1)
-push(2)
-push(3)
-push(4)
-push(5)
-push(6)
-print(a)
-print(pop())
-print(pop())
-print(a)
 
